f243c4316f25f5ad5df8f4ae15071d7d78bd69cf
e-wach <e_wangting@hotmail.com>
2024-12-06 11:25:34 +0100

    info distritos

---
+++ distritos_nomeclatura.py
@@ -0,0 +1,97 @@
+cudis_name = {
+    1: {
+        'name': 'CIUTAT VELLA',
+        'cudis': 4625001,
+        'codigos_postales': ['46001', '46002', '46003']
+    },
+    2: {
+        'name': "L'EIXAMPLE",
+        'cudis': 4625002,
+        'codigos_postales': ['46006', '46004', '46005']
+    },
+    3: {
+        'name': 'EXTRAMURS',
+        'cudis': 4625003,
+        'codigos_postales': ['46007', '46008']
+    },
+    4: {
+        'name': 'CAMPANAR',
+        'cudis': 4625004,
+        'codigos_postales': ['46015', '46009']
+    },
+    5: {
+        'name': 'LA SAIDIA',
+        'cudis': 4625005,
+        'codigos_postales': ['46009', '46010', '46019']
+    },
+    6: {
+        'name': 'EL PLA DEL REAL',
+        'cudis': 4625006,
+        'codigos_postales': ['46010', '46021']
+    },
+    7: {
+        'name': "L'OLIVERETA",
+        'cudis': 4625007,
+        'codigos_postales': ['46014', '46018']
+    },
+    8: {
+        'name': 'PATRAIX',
+        'cudis': 4625008,
+        'codigos_postales': ['46018', '46017', '46015']
+    },
+    9: {
+        'name': 'JESUS',
+        'cudis': 4625009,
+        'codigos_postales': ['46007', '46017', '46006']
+    },
+    10: {
+        'name': 'QUATRE CARRERES',
+        'cudis': 4625010,
+        'codigos_postales': ['46006', '46026', '46013']
+    },
+    11: {
+        'name': 'POBLATS MARITIMS',
+        'cudis': 4625011,
+        'codigos_postales': ['46024', '46011']
+    },
+    12: {
+        'name': 'CAMINS AL GRAU',
+        'cudis': 4625012,
+        'codigos_postales': ['46022', '46023']
+    },
+    13: {
+        'name': 'ALGIROS',
+        'cudis': 4625013,
+        'codigos_postales': ['46021', '46022']
+    },
+    14: {
+        'name': 'BENIMACLET',
+        'cudis': 4625014,
+        'codigos_postales': ['46020', '46022']
+    },
+    15: {
+        'name': 'RASCANYA',
+        'cudis': 4625015,
+        'codigos_postales': ['46019', '46025']
+    },
+    16: {
+        'name': 'BENICALAP',
+        'cudis': 4625016,
+        'codigos_postales': ['46015', '46035']
+    },
+    17: {
+        'name': 'POBLES DEL NORD',
+        'cudis': 4625017,
+        'codigos_postales': ['46016']
+    },
+    18: {
+        'name': "POBLES DE L'OEST",
+        'cudis': 4625018,
+        'codigos_postales': ['46035']
+    },
+    19: {
+        'name': 'POBLES DEL SUD',
+        'cudis': 4625019,
+        'codigos_postales': ['46012']
+    }
+}
